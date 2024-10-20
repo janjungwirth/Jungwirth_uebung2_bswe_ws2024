99d720fdd89f92be8fbbf6e6257d8ee90920efa1
Leon <160027005+LeonFHBurgenland@users.noreply.github.com>
2024-10-20 21:57:15 +0200

    Adapted Driver;

--- src/main/java/at/fh_burgenland/bswe/algo/Driver.java
+++ src/main/java/at/fh_burgenland/bswe/algo/Driver.java
@@ -1,6 +1,7 @@
 package at.fh_burgenland.bswe.algo;
 
 import at.fh_burgenland.bswe.algo.binarySearchIterativ.BinarySearchIterativ;
+import at.fh_burgenland.bswe.algo.selfOrganizingListT.SelfOrganizingListT;
 import lombok.extern.log4j.Log4j2;
 
 import java.util.Arrays;
@@ -9,55 +10,87 @@ import java.util.Scanner;
 @Log4j2
 public class Driver {
 
+    final static Scanner scanner = new Scanner(System.in);
+    final static int[] intList = {1, 2, 5, 7, 9, 11, 13, 15, 17, 19};
+    final static SelfOrganizingListT<Integer> listST = new SelfOrganizingListT<>();
+
     public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            listST.insert(i);
+        }
 
+        while (true) {
+            System.out.println("Willkommen im Suchalgorithmus-Menü!");
+            System.out.println("Bitte wählen Sie eine der folgenden Optionen:");
+            System.out.println("1: Iterative Binäre Suche");
+            System.out.println("2: Rekursive Binäre Suche");
+            System.out.println("3. Selbstanordnende lineare Liste Transpose");
+            System.out.println("0: Beenden");
 
-        int[] Array = {1, 2, 5, 7, 9, 11, 13, 15, 17, 19};
+            int userInput = scanner.nextInt();
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Willkommen im Suchalgorithmus-Menü!");
-        System.out.println("Bitte wählen Sie eine der folgenden Optionen:");
-        System.out.println("1: Iterative Binäre Suche");
-        System.out.println("2: Rekursive Binäre Suche");
-        System.out.println("3. Selbstanordnende lineare Liste Transpose");
-        System.out.println("0: Beenden");
-
-        int userInput = scanner.nextInt();
-
-        while (userInput != 0) {
-            System.out.print("Geben Sie den zu suchenden Wert ein: ");
-            System.out.println(Arrays.toString(Array));
-            int gesuchterWert = scanner.nextInt();
-
-            // Erstelle Instanzen der Suchklassen
-            BinarySearchIterativ iterativeSearch = new BinarySearchIterativ();
-            //TODO: Die anderen Klassen instanzieren
-
-
-
-            // Auswahl der Suchmethode
             switch (userInput) {
                 case 1:
-                    int resultIterative = iterativeSearch.binarySearchIterativ(Array, gesuchterWert);
-                    if (resultIterative != -1) {
-                        System.out.println("Gefunden an Index: " + resultIterative);
-                    } else {
-                        System.out.println("Nicht gefunden");
-                    }
-                    break;
-
+                    submenuBinaryItt();
                 case 2:
                     //TODO: Rekursive binäre Suche
                     break;
                 case 3:
-                    //TODO: Selbstanordnende lineare Liste Transpose
+                    SubmenuSelfOrganizeT();
                     break;
+                case 0:
+                    System.exit(0);
                 default:
                     System.out.println("Ungültige Auswahl, bitte versuch es nochmals.");
                     break;
             }
         }
-        System.out.println("Programm beendet.");
+
+    }
+
+    private static void SubmenuSelfOrganizeT() {
+        while (true) {
+            System.out.print("Geben Sie den zu suchenden Wert ein, oder X um zu beenden: ");
+            listST.printToConsole();
+            String input = scanner.next();
+            int gesuchterWert;
+            if (input.equalsIgnoreCase("x")) break;
+            try {
+                gesuchterWert = Integer.parseInt(input);
+            } catch (Exception ignored) {
+                continue;
+            }
+            Integer wert = listST.searchTranspose(gesuchterWert);
+            if (wert != null) {
+                System.out.println(wert + " gefunden und ggf. um ein Element nach vorne verschoben:");
+                listST.printToConsole();
+            } else {
+                System.out.println("Kein ergebniss.");
+            }
+        }
+
+    }
+
+    private static void submenuBinaryItt() {
+        while (true) {
+            System.out.print("Geben Sie den zu suchenden Wert ein, oder X um zu beenden: ");
+            System.out.println(Arrays.toString(intList));
+            String input = scanner.next();
+            int gesuchterWert;
+            if (input.equalsIgnoreCase("x")) break;
+            try {
+                gesuchterWert = Integer.parseInt(input);
+            } catch (Exception ignored) {
+                continue;
+            }
+
+            BinarySearchIterativ iterativeSearch = new BinarySearchIterativ();
+            int resultIterative = iterativeSearch.binarySearchIterativ(intList, gesuchterWert);
+            if (resultIterative != -1) {
+                System.out.println("Gefunden an Index: " + resultIterative);
+            } else {
+                System.out.println("Nicht gefunden");
+            }
+        }
     }
 }
